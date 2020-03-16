62b709d839084d2b1606304ccf0d2619b4f940bf
iray100200 <iray100200@hotmail.com>
2020-03-17 00:48:13 +0800

    update

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ import theme from '../components/theme'
 import { CacheProvider } from '../components/providers'
 import cookie from 'cookie'
 import Notification from '../components/notification'
+import CssBaseline from '@material-ui/core/CssBaseline'
 
 export default class MyApp extends App {
   componentDidMount() {
@@ -21,6 +22,7 @@ export default class MyApp extends App {
     cookieData.userName = cookieData.username && Buffer.from(cookieData.username, 'base64').toString('utf8')
     return (
       <React.Fragment>
+        <CssBaseline />
         <Head>
           <title>镇江市专利网站</title>
         </Head>
