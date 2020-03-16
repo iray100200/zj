bef5172294eb43f6e090a70b02ed7ea9c63eac25
iray100200 <iray100200@hotmail.com>
2020-03-17 01:13:20 +0800

    update

--- next.config.js
+++
@@ -1,5 +0,0 @@
-const withLess = require("@zeit/next-less")
-const withCss = require("@zeit/next-css")
-const withPlugins = require("next-compose-plugins")
-
-module.exports = withPlugins([withLess, withCss])
