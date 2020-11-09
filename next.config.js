aaef699dea71e777901b0ba7cc98c1377f7c652c
iray100200 <iray100200@hotmail.com>
2020-11-10 00:08:32 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,32 @@
-const withCSS = require('@zeit/next-css')
-
+const withCss = require('@zeit/next-css')
+const path = require('path')
+const cssLoaderGetLocalIdent = require('css-loader/lib/getLocalIdent.js')
+ 
 module.exports = {
-  ...withCSS({
-    cssModules: true,
-  }),
   env: {
     APP_URL: 'http://114.215.179.120:8080/netcrawl'
-  }
+  },
+  ...withCss({
+    cssModules: true,
+    cssLoaderOptions: {
+      localIdentName: '[local]___[hash:base64:5]',
+      getLocalIdent: (context, localIdentName, localName, options) => {
+        const hz = context.resourcePath.replace(context.rootContext, '')
+        console.log(hz)
+        if (/node_modules/.test(hz)) {
+          return localName
+        }
+        return cssLoaderGetLocalIdent(context, localIdentName, localName, options)
+      },
+    },
+    distDir: './.next',
+    webpack: (config, options) => {
+      // Fixes npm packages that depend on `fs` module
+      config.node = {
+        fs: 'empty',
+      }
+      config.resolve.alias['@'] = path.join(__dirname)
+      return config
+    },
+  })
 }
